c66a612ac17fc95abc60ede4d14c39b6002018f2
Léon Kiefer <leon.kiefer@proton.me>
2024-01-12 16:47:09 +0100

    Update imports
    Update imports and create instance of `TerminalPrinter`

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # main.py
 from netscan.scanner import NetworkScanner
-from netscan.display import print_results
+from netscan.printer import TerminalPrinter
 
 
 def main():
     network_range = "192.168.1.0/24"
+
+    # Create an instance of NetworkScanner
     scanner = NetworkScanner(network_range)
     results = scanner.initial_scan()
-    print_results(results)
+
+    # Create an instance of TerminalPrinter
+    printer = TerminalPrinter()
+    printer.print_results(results)
 
 
 if __name__ == "__main__":
--- netscan/display.py
+++
@@ -1,8 +0,0 @@
-# netscan/display.py
-from tabulate import tabulate
-from termcolor import colored
-
-
-def print_results(results):
-    headers = ["IP Address", "MAC Address", "Possible OS"]
-    print(colored(tabulate(results, headers=headers, tablefmt="grid"), "green"))
